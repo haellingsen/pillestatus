59a9b67843afbea269d1ab7db49ec08d7e7396ac
Harald Ellingsen <ellingsen.harald@gmail.com>
2022-10-05 15:04:05 +0200

    cleanup

--- main.py
+++ main.py
@@ -73,26 +73,6 @@ def main(thresh_1=10, thresh_2=100):
         #print(M.keys())
 
 
-        # test from https://pyimagesearch.com/2016/04/04/measuring-distance-between-objects-in-an-image-with-opencv/
-        edged = cv2.Canny(img_gray, thresh_1, thresh_2)
-        edged = cv2.dilate(edged, None, iterations=1)
-        edged = cv2.erode(edged, None, iterations=1)
-
-        # find contours in the edge map
-        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        # sort the contours from left-to-right and, then initialize the
-        # distance colors and reference object
-        (cnts, _) = contours.sort_contours(cnts)
-
-        img_copy = frame.copy()
-        edged = cv2.drawContours(img_copy, contours=cnts,
-                                     contourIdx=-1,
-                                     color=(255, 0, 0), thickness=2)
-
-
-
 
         # The centroid point
         cx = int(M['m10'] / M['m00'])
@@ -107,15 +87,6 @@ def main(thresh_1=10, thresh_2=100):
         xcor = [p[0] for p in pst]
         ycor = [p[1] for p in pst]
 
-        # Plot the points
-        # plt.figure(figsize=(16, 8))
-        # plt.subplot(3, 2, 1)
-        # plt.imshow(img_gray, cmap='gray')
-        # plt.scatter([cx], [cy], c='b', s=50)
-        # plt.subplot(3, 2, 2)
-        # plt.imshow(img_gray, cmap='gray')
-        # plt.scatter(xcor, ycor, c='b', s=50)
-     #   plt.waitforbuttonpress(1)
 
         # The first order of the contours
         c_0 = contours_[0]
@@ -136,10 +107,6 @@ def main(thresh_1=10, thresh_2=100):
                                      contourIdx=-1,
                                      color=(255, 0, 0), thickness=2)
 
-        # plt.subplot(3, 2, 3);
-        # plt.imshow(img_box)
-        # plt.subplot(3, 2, 4);
-        # plt.imshow(img_box_2)
 
         # Detect the convex contour
         hull = cv2.convexHull(c_0)
@@ -147,17 +114,6 @@ def main(thresh_1=10, thresh_2=100):
         img_hull = cv2.drawContours(img_copy, contours=[hull],
                                     contourIdx=0,
                                     color=(255, 0, 0), thickness=2)
-        # plt.subplot(3, 2, 5)
-        # plt.imshow(img_hull)
-        # plt.waitforbuttonpress(1)
-        #
-        # f, axarr = plt.subplots(2, 2, figsize=(16, 8))
-        # axarr[0, 0].imshow(img_blur)
-        # axarr[0, 1].imshow(img_gray)
-        # axarr[1, 0].imshow(thresh, cmap='gray')
-        # axarr[1, 1].imshow(final)
-        # plt.waitforbuttonpress(1)
-        # input()
 
         font = cv2.FONT_HERSHEY_SIMPLEX
         fontScale = 1.5
@@ -165,8 +121,7 @@ def main(thresh_1=10, thresh_2=100):
         thickness = 3
         lineType = 2
 
-        text_to_put = str(f"{current_area:.0f}")
-        text_to_put_convex_hull = str(f"{cv2.contourArea(big_contour):.0f}")
+        text_to_put = str(f"{w * h:.0f}")
 
         textwidth = cv2.getTextSize(text_to_put,
                     font,
@@ -176,15 +131,7 @@ def main(thresh_1=10, thresh_2=100):
         bottomLeftCornerOfText = int(x + w/2 - textwidth/2), int(y + h/2)
 
 
-        cv2.putText(img_box_2, text_to_put,
-                    bottomLeftCornerOfText,
-                    font,
-                    fontScale,
-                    fontColor,
-                    thickness,
-                    lineType)
-
-        cv2.putText(edged, text_to_put_convex_hull,
+        cv2.putText(img_box, text_to_put,
                     bottomLeftCornerOfText,
                     font,
                     fontScale,
@@ -193,10 +140,7 @@ def main(thresh_1=10, thresh_2=100):
                     lineType)
 
 
-
-        # concatenate image Horizontally
-        hori = np.concatenate((final, edged, img_box_2), axis=1)
-        cv2.imshow('VIDEO', hori)
+        cv2.imshow('VIDEO', img_box)
         cv2.waitKey(1)
 
         now = datetime.datetime.now()
